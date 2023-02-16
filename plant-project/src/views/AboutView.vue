933e4e7e4268cb2611d5a91360fbe5cded0e5e80
giorgia paparazzo <giorgia.paparazzo@joyflo.com>
2023-02-16 17:48:40 +0100

    get single value from api response

--- plant-project/src/views/AboutView.vue
+++ plant-project/src/views/AboutView.vue
@@ -1,6 +1,30 @@
+<script>
+import axios from 'axios';
+import { ref, watch } from 'vue';
+export default {
+  setup() {
+    const latitude = ref('')
+    return {
+      latitude
+    }
+  },
+  mounted() {
+    axios
+      // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      // .then(response => console.log(response.data))
+
+      .get('https://api.open-meteo.com/v1/forecast?latitude=45.41&longitude=11.89&hourly=temperature_2m')
+      // .then(response => console.log(response.data))
+      .then(response => (this.latitude = response.data.latitude))
+
+  }
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ latitude }}</p>
   </div>
 </template>
 
