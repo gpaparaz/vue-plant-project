67b05fbb718d17301cf81c234e52eaf6eecb30fb
giorgia paparazzo <giorgia.paparazzo@joyflo.com>
2023-02-20 10:11:58 +0100

    modified api call
    modificata la api in maniera tale che chiami solo oggi e non tutta la settimana, e dalla chiamata estrapoli dati. 
    definita anche la lista dei codici meteo

--- plant-project/src/objects/datagenerator.js
+++ plant-project/src/objects/datagenerator.js
@@ -7,3 +7,32 @@ export let collectionData = [
     new Plant(id++, 'Glicine', 'Viola', 4, 'https://assets.afcdn.com/story/20210802/2130668_w2121h2121c1cx1642cy627cxt0cyt0cxb2121cyb1414.jpg'),
     new Plant(id++, 'Edera', 'Canadese', 2, 'https://www.giardinaggio.it/giardino/rampicanti/vite-canadese_O3.jpg')
 ];
+
+export let codiceMeteo = [
+    { "0": "Cielo sereno" },
+    { "1": "Prevalentemente chiaro" },
+    { "2": "parzialmente nuvoloso" },
+    { "3": "coperto" },
+    { "45": "Nebbia" },
+    { "48": "nebbia di brina" },
+    { "51": "Pioggia" },
+    { "53": "Pioggia leggera" },
+    { "55": "Pioggia densa intensità" },
+    { "56": "Pioggia gelata" },
+    { "57": "Pioggia gelata densa intensità" },
+    { "61": "Pioggia" },
+    { "63": "Pioggia moderata" },
+    { "65": "Pioggia forte" },
+    { "66": "Pioggia gelida" },
+    { "67": "Pioggia gelata intensa" },
+    { "71": "Nevicata" },
+    { "73": "Caduta di neve moderata" },
+    { "77": "Granelli di neve" },
+    { "80": "Pioggia leggera" },
+    { "81": "Rovesci di pioggia moderati" },
+    { "82": "Pioggia violenta" },
+    { "85": "Docce di neve leggere" },
+    { "86": "Rovesci di neve forti" },
+    { "95": "Temporale" },
+    { "96": "Temporale moderato" },
+    { "99": "Temporale intenso" }]
--- plant-project/src/views/AboutView.vue
+++ plant-project/src/views/AboutView.vue
@@ -1,21 +1,43 @@
 <script>
 import axios from 'axios';
 import { ref, watch } from 'vue';
+import { codiceMeteo } from '../objects/datagenerator'
 export default {
   setup() {
     const latitude = ref('')
+    const tMin = ref('')
+    const tMax = ref('')
+    const meteo = ref('')
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + "-" + mm + "-" + dd;
+
+    const oggi = ref(today)
     return {
-      latitude
+      latitude, tMin, tMax, oggi
     }
   },
   mounted() {
+    function getToday() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = today.getFullYear();
+
+      today = yyyy + "-" + mm + "-" + dd;
+      return today;
+    }
+
     axios
       // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       // .then(response => console.log(response.data))
 
-      .get('https://api.open-meteo.com/v1/forecast?latitude=45.41&longitude=11.89&hourly=temperature_2m')
-      // .then(response => console.log(response.data))
-      .then(response => (this.latitude = response.data.latitude))
+      .get('https://api.open-meteo.com/v1/forecast?latitude=45.41&longitude=11.89&hourly=temperature_2m,weathercode&start_date=' + getToday() + '&end_date=' + getToday())
+      .then(response => (this.latitude = response.data.latitude) && (this.tMax = response.data.hourly.temperature_2m[response.data.hourly.temperature_2m.length - 1]) && (this.tMin = response.data.hourly.temperature_2m[0]) && console.log(response.data))
 
   }
 }
@@ -24,10 +46,11 @@ export default {
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>{{ latitude }}</p>
+    <h3>Che tempo fa a Padova?</h3>
+    <p>Min: {{ tMin }} Max: {{ tMax }}</p>
+    <img src="" />
+    <p>{{ oggi }}</p>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
